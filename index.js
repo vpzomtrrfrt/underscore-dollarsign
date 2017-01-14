70533e3702e9310b62981d38829ab95606ee50a4
Colin Reeder <vpzomtrrfrt@gmail.com>
2017-01-14 15:21:42 -0700

    More features, proxy fallback

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 (function() {
+	var suppressedWarnings = [];
+	var plugins = [];
+
+	function isNode(o){
+		return (
+			typeof Node === "object" ? o instanceof Node : 
+				o && typeof o === "object" && typeof o.nodeType === "number" && typeof o.nodeName==="string"
+		);
+	}
+
 	function setup(list) {
 		var tr = {};
 		function setValue(key, value) {
@@ -6,7 +16,7 @@
 				list[i][key] = value;
 			}
 		}
-		function getValue(key, info, value) {
+		function getValue(key, info) {
 			if(list.length == 1) {
 				return list[0][key];
 			}
@@ -26,12 +36,75 @@
 			Object.defineProperty(tr, key, basic(key));
 		}
 		setBasic("textContent");
+		setBasic("innerHTML");
+
+		tr.is_$ = true;
+
+		function callMethod(name) {
+			var value = "";
+			for(var i = 0; i < list.length; i++) {
+				value += list[i][name].apply(list[i], [].slice.call(arguments, 1));
+			}
+			return value;
+		}
+		function setMethod(key) {
+			tr[key] = callMethod.bind(this, key);
+		}
+
+		setMethod("addEventListener");
+
+		for(var i = 0; i < plugins.length; i++) {
+			var plugin = plugins[i];
+			if(typeof plugin.extraFields === "object") {
+				for(var key in plugin.extraFields) {
+					tr[key] = plugin.extraFields[key];
+				}
+			}
+			if(typeof plugin.applyToInstance === "function") {
+				plugin.applyToInstance(tr);
+			}
+		}
+
+		tr.__noSuchMethod__ = function(id, args) {
+			return callMethod.apply(this, id, args);
+		};
+
+		if(typeof Proxy === "function") {
+			function proxyWarning(name) {
+				if(suppressedWarnings.indexOf(name) > -1) {
+					return;
+				}
+				console.warn("Using proxy, as \""+name+"\" is not provided explicitly by _$.  This may not work in some browsers.  Report this and/or use _$.suppressProxyWarning");
+			}
+			tr = new Proxy(tr, {
+				get: function(target, name) {
+					if(name in target) return target[name];
+					proxyWarning(name);
+					return getValue(name, null);
+				},
+				set: function(target, name, value) {
+					if(name in target) target[name] = value;
+					else {
+						proxyWarning(name);
+						setValue(name, value);
+					}
+				}
+			});
+		}
 		return tr;
 	}
 
 	var _$ = function(query) {
 		var typ = typeof query;
 		if(typ === "object") {
+			if(query.is_$) {
+				// already a _$ object
+				return query;
+			}
+			if(isNode(query)) {
+				// single element, make it an array
+				return _$([query]);
+			}
 			// probably an array
 			return setup([].slice.call(query));
 		}
@@ -43,7 +116,18 @@
 		}
 	};
 
+	_$.suppressProxyWarning = function(name) {
+		suppressedWarnings.push(name);
+	};
+
+	_$.addPlugin = function(plugin) {
+		plugins.push(plugin);
+	};
+
 	if(typeof window === "object") {
 		window._$ = _$;
+	}
+	if(typeof module === "object") {
+		module.exports = _$;
 	}
 })();
